b914f7cf0ddcbe4d29b35f924928e40534c4aef8
Amy <amysuelumpkin@gmail.com>
2018-03-13 20:30:54 -0400

    server

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,3 +1,28 @@
 //import express
 //import burger.js
 //create the router and export the router at end of file
+
+const express = require("express");
+
+const router = express.Router();
+
+//import model burger.js to use its database functions
+//const burger = require("../models/burger");
+
+router.get("/", function(req, res) {
+    burgers.all(function(err, data) {
+        if(err) res.status(500).send(err);
+        else {
+            const hbsObject = { burgers: data };
+            console.log(hbsObject);
+            res.render("index", hbsObject); //render index hb file
+        }
+    });
+
+
+
+
+
+//res.send("You're on the home page");// change this to res render index
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,2 +1,25 @@
 const express = require("express");
-const body-parser = require("body-parser");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended:true }));
+
+app.use(bodyParser.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
+app.set("view engine", "hbs");
+
+const burgerRoutes = require("./controllers/burgersController");
+app.use("/burgers", burgerRoutes); //requests have to come from my website/burgers...this is the home page
+
+app.get("/", (req, res) => { res.redirect("/burgers")}); 
+
+const PORT = process.env.PORT || process.argv[2] || 8080;
+app.listen(PORT, function(){
+    console.log("App is listening on port: " + PORT);
+});
